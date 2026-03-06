f7cf4a316de44de8482ea8aec721d185c3f2fafa
HibatullahiShakira <HibatullahiShakira@gmail.com>
2026-03-06 12:07:17 +0100

    Changed Refersh token max length from 255

--- apps/core/migrations/0001_initial.py
+++ apps/core/migrations/0001_initial.py
@@ -60,7 +60,7 @@ class Migration(migrations.Migration):
             name='RefreshToken',
             fields=[
                 ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
-                ('token', models.CharField(db_index=True, max_length=255, unique=True)),
+                ('token', models.CharField(db_index=True, max_length=512, unique=True)),
                 ('expires_at', models.DateTimeField()),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('revoked', models.BooleanField(default=False)),
--- apps/core/models.py
+++ apps/core/models.py
@@ -80,7 +80,7 @@ class RefreshToken(models.Model):
     """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="refresh_tokens")
-    token = models.CharField(max_length=255, unique=True, db_index=True)
+    token = models.CharField(max_length=512, unique=True, db_index=True)
     expires_at = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
     revoked = models.BooleanField(default=False)
